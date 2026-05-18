683a5492044df836f414218a3e52a3a3bde465bd
liuyang <liuyang19@corp.netease.com>
2026-05-19 00:20:28 +0800

    Initial Commit

---
+++ vbcc-mac/vbcc_macApp.swift
@@ -0,0 +1,32 @@
+//
+//  vbcc_macApp.swift
+//  vbcc-mac
+//
+//  Created by yang on 2026/5/19.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct vbcc_macApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ vbcc-macTests/vbcc_macTests.swift
@@ -0,0 +1,17 @@
+//
+//  vbcc_macTests.swift
+//  vbcc-macTests
+//
+//  Created by yang on 2026/5/19.
+//
+
+import Testing
+@testable import vbcc_mac
+
+struct vbcc_macTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
